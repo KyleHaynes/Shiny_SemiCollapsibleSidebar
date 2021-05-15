1c94d919449c0b70f60dec37e12027e68cd17b42
KyleHaynes <kyle@kylehaynes.com.au>
2021-05-15 16:46:54 +1000

    changes

--- server.R
+++ server.R
@@ -1,10 +1,22 @@
 library(shiny)
 library(DT)
 library(data.table)
+library(generator)
 
-d <- iris
-d <- data.frame(lapply(d, as.character), stringsAsFactors=FALSE)
-setDT(d)
+# n = 10000
+# d <- data.table(name = toupper(r_full_names(n))
+#              , alias_name_1 = toupper(r_full_names(n))
+#              , alias_name_2 = toupper(r_full_names(n))
+#              , alias_name_3 = toupper(r_full_names(n))
+#              , phone_1 = r_phone_numbers(n)
+#              , phone_2 = r_phone_numbers(n)
+#              , phone_3 = r_phone_numbers(n)
+#              , dob = r_date_of_births(n, start = as.Date("1900-01-01"), end = Sys.Date()))
+
+# d <- iris
+# d <- data.frame(lapply(d, as.character), stringsAsFactors=FALSE)
+# setDT(d)
+
 
 app = shinyApp(
  ui =
@@ -20,22 +32,49 @@ app = shinyApp(
                   bsCollapsePanel("Panel 1", "This is a panel with just text ",
                     "and has the default style. You can change the style in ",
                     "the sidebar.", style = "info"
+                    # ---- Subset Vars ----
                     , "Subset vars:"
-                    , selectInput("sub_vars", NULL, names(d), selected = names(d)[1], multiple = T)
+                    , selectInput("sub_vars", NULL, names(d), selected = names(d), multiple = T, selectize = T)
                     , "Search"
-                    , selectInput("var_1", NULL, names(d), selected = names(d)[1])
-                    , textInput("val_1", NULL, value = "", width = NULL, placeholder = NULL)
-                    , selectInput("var_2", NULL, names(d), selected = names(d)[2])
-                    , textInput("val_2", NULL, value = "", width = NULL, placeholder = NULL)
-                    , selectInput("var_3", NULL, names(d), selected = names(d)[3])
-                    , textInput("val_3", NULL, value = "", width = NULL, placeholder = NULL)
+                    
+                    # ---- First search
+                    , fluidRow(
+                        column(1,
+                        selectInput("var_1", NULL, names(d), selected = names(d)[1], multiple = T, width = "200px")
+                        ),
+                        column(10, # offset = 4,
+                        textInput("val_1", NULL, value = "", width = NULL, placeholder = NULL)
+                        )      
+                    )
+
+                    # ---- Second search
+                    , fluidRow(
+                        column(1,
+                        selectInput("var_2", NULL, names(d), selected = names(d)[2], multiple = T, width = "200px")
+                        ),
+                        column(2, # offset = 4,
+                        textInput("val_2", NULL, value = "", width = NULL, placeholder = NULL)
+                        )      
+                    )
+
+                    # ---- Third search
+                    , fluidRow(
+                        column(1,
+                        selectInput("var_3", NULL, names(d), selected = names(d)[3], multiple = T, width = "200px")
+                        ),
+                        column(2, # offset = 4,
+                        textInput("val_3", NULL, value = "", width = NULL, placeholder = NULL)
+                        )      
+                    )
+
+                    , textInput("threshold", "Threshold:", value = "1", width = NULL, placeholder = NULL)
                     , actionButton("action", label = "Action")
 
                     # End of panel 1 
                   ),
                   bsCollapsePanel("Panel 2", "This panel has a generic plot. ", "and a 'success' style.", 
                    # Panel two shit                  
-                   DT::dataTableOutput("mytable")
+                  DT::dataTableOutput("mytable")
        )
      )
    )
@@ -61,23 +100,36 @@ app = shinyApp(
         d[, l := F]
 
         if(input$val_1 != ""){
-            d[, l := l + (eval(as.name(input$var_1)) %ilike% input$val_1)]
+            cnt_1 <- c()
+            for(i in 1:length(input$var_1)){
+                browser()
+                d[, l := l + (x <<- (eval(as.name(input$var_1)) %ilike% input$val_1))]
+                cnt_1 <- c(cnt_1, sum(x))
+            }
+            output$results_1 <- renderText({as.character(paste(cnt_1, collapse = ", "))})
         }
 
         if(input$val_2 != ""){
-            d[, l := l + (eval(as.name(input$var_2)) %ilike% input$val_2)]
+            for(i in 1:length(input$var_2)){
+                d[, l := l + (eval(as.name(input$var_2)) %ilike% input$val_2)]
+            }
         }
-        
+
         if(input$val_3 != ""){
-            d[, l := l + (eval(as.name(input$var_3)) %ilike% input$val_3)]
+            for(i in 1:length(input$var_3)){
+                d[, l := l + (eval(as.name(input$var_3)) %ilike% input$val_3)]
+            }
         }
-                
 
 
         vars <- input$sub_vars
+        # browser()
+        if(is.null(length(vars))){
+            vars <- names(d)[1:2]
+        }
 
         output$mytable = DT::renderDataTable({
-            d[l >= 1, ..vars]
+            d[l >= as.numeric(input$threshold), ..vars]
         })
 
     })
